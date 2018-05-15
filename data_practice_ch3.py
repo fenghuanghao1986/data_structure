a8c4087e2af1bfa104d4da345418c503f341acc7
FingerStyleFinn <hfeng7@du.edu>
2018-05-15 13:35:31 -0600

    learning Queue
    see comments in the code
    isEmpty is not working.

---
+++ data_practice_ch3.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 15 12:40:49 2018
+
+@author: fengh
+"""
+
+# queue practice
+
+class Queue:
+    def __init__(self):
+        self.items = []
+    # it gives me Queue object has no attribute 'items'
+    # no matter if the object I created is empty or not.
+    def isEmpty(self):
+        return self.itmes == []
+    
+    def enqueue(self, item):
+        self.items.insert(0, item)
+        
+    def dequeue(self):
+        return self.items.pop()
+    
+    def size(self):
+        return len(self.items)
+    
+# hot potato
+        
