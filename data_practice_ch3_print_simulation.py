16ef30890e6a85106307a9f721eee5b1e119e61b
FingerStyleFinn <hfeng7@du.edu>
2018-05-22 22:12:16 -0600

    add code
    code not work

--- data_practice_ch3_print_simulation.py
+++ data_practice_ch3_print_simulation.py
@@ -44,3 +44,58 @@ class Task:
     def waitTime(self, currenttime):
         return currenttime - self.timestamp
     
+from pythonds.basic.queue import Queue
+
+import random
+
+def simulation(numSeconds, pagesPerMinute):
+    
+    labprinter = Printer(pagesPerMinute)
+    printQueue = Queue()
+    waitingtimes = []
+    
+    for currentSecond in range(numSeconds):
+        
+        if newPrintTask():
+            task = Task(currentSecond)
+            printQueue.enqueue(task)
+            
+        if (not labprinter.busy()) and (not printQueue.isEmpty()):
+            nexttask = printQueue.dequeue()
+            waitingtimes.append(nexttask.waitTime(currentSecond))
+            labprinter.startNext(nexttask)
+            
+        labprinter.tick()
+        
+    averageWait = sum(waitingtimes) / len(waitingtimes)
+    print("Average Wait %6.2f secs %3d tasks remaining."%(averageWait,printQueue.size()))
+    
+def newPrintTask():
+    num = random.randrange(1, 181)
+    if num == 180:
+        return True
+    else:
+        return False
+    
+for i in range(10):
+    simulation(3600, 5)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
